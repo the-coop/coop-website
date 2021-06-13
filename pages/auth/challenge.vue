22cb7d88b4926ab5084023f8fc2b0e41a4ccf472
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-13 23:37:32 +0100

    Improve slightly.

--- pages/auth/challenge.vue
+++ pages/auth/challenge.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="challenge-wrapper">
-    <h1>Authenticating</h1>
-    <p>Attemping to verify your Discord account in exchange for greater access.</p>
-    <div v-if="loaded">
-      <h2>Welcome {{this.username}}!</h2>
-      <p>We value privacy so this session will be forgotten/discarded when you close your browser, if you wish to be remembered for convenience - click here: <a href="upgrade-session">upgrade session/remember me.</a></p>
-      <img class="profile-image" src="" />
+    <div v-if="!loaded">
+      <h1>Authenticating</h1>
+      <p>Attemping to verify your Discord account in exchange for greater access.</p>
     </div>
-    <div v-if="error">
+
+    <h1 v-if="loaded && !error">
+      <h2>Welcome {{this.username}}!</h2>
+      <p>We value privacy so this session will be forgotten/discarded when you close your browser, if you wish to be remembered for convenience - click here:</p>
+      <button class="upgrade-session">Remember me! :D</button>
+
+      <img class="profile-image" src="" />
+    </h1>
+
+    <div v-if="loaded && error">
       <h2>Error authenticating.</h2>
       {{error}}
     </div>
@@ -16,7 +22,7 @@
 
 <script>
   import Auth from '~/lib/auth/auth';
-  import API from '../../lib/api/api';
+  import API from '~/lib/api/api';
 
   export default {
     mounted() {
@@ -36,8 +42,6 @@
           // Access the authorisation params provided by OAuth redirect.
           const fragment = new URLSearchParams(window.location.hash.slice(1));
 
-          console.log(fragment.get('token_type'));
-
           // Guard against no token at all.
           if (!fragment.get('access_token')) throw new Error('No code passed.');
 
@@ -46,12 +50,9 @@
 
           // Assert that it's actually valid.
           const me = await API.get_json('https://discord.com/api/users/@me');
-          console.log(me);
-
           if (!me) throw new Error('Invalid user/token.');
 
           // Set the username for a visual feedback.
-          console.log('valid user', me);
           this.username = me.username;
 
           // Set as loaded.
