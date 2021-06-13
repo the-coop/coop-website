985ca3d56802f43a9a8c1148e19352987c235ec9
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-13 23:01:01 +0100

    Test...

--- pages/auth/challenge.vue
+++ pages/auth/challenge.vue
@@ -3,12 +3,12 @@
     <h1>Authenticating</h1>
     <p>Attemping to verify your Discord account in exchange for greater access.</p>
     <div v-if="loaded">
-      <h2>Welcome {this.username}!</h2>
+      <h2>Welcome {{this.username}}!</h2>
       <img class="profile-image" src="" />
     </div>
     <div v-if="error">
       <h2>Error authenticating.</h2>
-      {error}
+      {{error}}
     </div>
   </div>
 </template>
@@ -33,14 +33,23 @@
         // Access the authorisation params provided by OAuth redirect.
         const fragment = new URLSearchParams(window.location.hash.slice(1));
 
+        console.log('authenticating');
+
         try {
           // Guard against no token at all.
+          console.log('testing access token');
           if (!fragment.get('access_token')) throw new Error('No code passed.');
+
+          console.log('access token exists');
 
           // Assert that it's actually valid.
           const me = await API.get_json('https://discord.com/api/users/@me');
           console.log(me);
+
           if (!me) throw new Error('Invalid user/token.');
+
+          // Set the username for a visual feedback.
+          console.log('valid user', me);
 
           // Put the token in session storage unless they specify remember me then -> local storage.
           localStorage.set('discord_access_token', fragment.get('access_token'));
