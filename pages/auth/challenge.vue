3f0cd862b319e3d5967f3ca4e18f1ab15d2a5299
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-13 22:32:34 +0100

    More login progress! :D

--- pages/auth/challenge.vue
+++ pages/auth/challenge.vue
@@ -1,19 +1,67 @@
 <template>
-  <h1>Attemping to verify your Discord account in exchange for greater access.</h1>
+  <div class="challenge-wrapper">
+    <h1>Authenticating</h1>
+    <p>Attemping to verify your Discord account in exchange for greater access.</p>
+    <div v-if="loaded">
+      <h2>Welcome ?!</h2>
+      <img class="profile-image" src="" />
+    </div>
+  </div>
 </template>
 
-
 <script>
+  const API_GET_JSON = async (url) => {
+    const response = await fetch(url, { headers: { authorization: window.authorization }});
+    const result = await response.json();
+    return result;
+  }
+
   export default {
-    mounted: () => {
-      alert('mounted');
+    mounted() {
       this.authenticate();
     },
+    data() {
+      return {
+        loaded: false
+      }
+    },
     methods: {
-      authenticate() {
-        // Access the code from query params and authenticate with it.
-        console.log('authenticating');
+       async authenticate() {
+        let error = null;
+
+        const fragment = new URLSearchParams(window.location.hash.slice(1));
+        const [ accessToken, tokenType ] = [fragment.get('access_token'), fragment.get('token_type')];
+        window.authorization = `${tokenType} ${accessToken}`;
+
+        // Exchange with Discord OAuth service for access token with data.
+
+
+        // Store globally.
         
+
+        // Put the token in session storage unless they specify remember me then -> local storage.
+        // TODO: ^
+
+        if (!accessToken) error = 'No code passed.';
+
+        try {
+          const result = await API_GET_JSON('https://discord.com/api/users/@me');
+
+          console.log(result);
+
+          const { username, discriminator } = result;
+
+          console.log(username, discriminator);
+          this.loaded = true;
+
+        } catch(e) {
+          error = e.message;
+        }
+
+        // If attempt failed
+        if (error) {
+          // Show error box and message.
+        }
       }
     }
   }
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="https://discord.com/api/oauth2/authorize?client_id=799695179623432222&redirect_uri=https%3A%2F%2Fthecoop.group%2Fauth%2Fchallenge&response_type=code&scope=identify">
+    <a href="https://discord.com/api/oauth2/authorize?client_id=799695179623432222&redirect_uri=https%3A%2F%2Fthecoop.group%2Fauth%2Fchallenge&response_type=token&scope=identify">
         LOGIN LOL
     </a>
 </template>
