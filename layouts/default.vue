950eb0c0a351d45fa32b364c6dc88937f973c3c0
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-19 00:52:22 +0100

    :shrug:

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,12 +16,12 @@
         <NuxtLink to="/conquest" class="nav-link">Conquest</NuxtLink>
         
         <!-- Actions for guests/non-users/logged out users -->
-        <NuxtLink v-if="!$auth.$state.loggedIn" to="/auth/login" class="nav-link">Login</NuxtLink>
-        <a v-if="!$auth.$state.loggedIn" href="https://discord.gg/2gTTUZbRVD" target="_blank" class="nav-link">Apply</a>
+        <NuxtLink v-show="!$auth.$state.loggedIn" to="/auth/login" class="nav-link">Login</NuxtLink>
+        <a v-show="!$auth.$state.loggedIn" href="https://discord.gg/2gTTUZbRVD" target="_blank" class="nav-link">Apply</a>
 
         <!-- Actions for logged un users -->
-        <NuxtLink v-if="$auth.$state.loggedIn" to="/profile" class="nav-link">Profile</NuxtLink>
-        <button v-if="$auth.$state.loggedIn" 
+        <NuxtLink v-show="$auth.$state.loggedIn" to="/profile" class="nav-link">Profile</NuxtLink>
+        <button v-show="$auth.$state.loggedIn" 
           class="nav-link"
           v-on:click="logout">Logout</button>
 
