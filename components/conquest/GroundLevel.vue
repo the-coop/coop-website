396eca4fa770ecdc3341954e00dd0654844067d8
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-23 20:36:30 +0100

    Add websocket.

---
+++ components/conquest/GroundLevel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="groundlevel" />
+</template>
+
+<style>
+  .groundlevel {
+    width: 100vw;
+    height: 100vh;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+</style>
+
+<script>
+  import * as THREE from 'three';
+  import { io } from "socket.io-client";  
+
+  export default {
+    name: 'GroundLevel',
+    props: {
+      tile: {
+        type: String,
+        default: null
+      }
+    },
+    async mounted() {
+      // Used for shared state.
+      window.GROUND_LEVEL = {};
+
+      // Connect to the website.
+      const socket = io("https://cooperchickenbot.herokuapp.com/");
+      
+    }
+  }
+</script>
---
+++ pages/conquest/ground.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+    components: {
+      'Worldview': () => import('@/components/conquest/GroundLevel.vue')
+    },
+    layout: 'fullscreen',
+    head() {
+      return {
+        title: 'Coop Conquest: Ground Level'
+      }
+    },
+    data() {
+      return {
+        tile: this.$nuxt.$route.query.tile
+      }
+    },
+    mounted() {
+      // console.log(this.tile);
+    }
+  }
+</script>
+
+<template>
+  <!-- Load map -->
+  <client-only><Worldview :tile="tile" /></client-only>
+</template>
