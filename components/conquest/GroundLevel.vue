36aace5a8c3af74787b33b75527d81c446b87319
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-23 20:58:09 +0100

    Generate some testing data.

--- components/conquest/GroundLevel.vue
+++ components/conquest/GroundLevel.vue
@@ -18,6 +18,21 @@
   import * as THREE from 'three';
   import { io } from "socket.io-client";  
 
+  const generateGround = () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+
+    // Set the size and append the element.
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // Globalise the ground/scene/core components for better access later.
+    window.GROUND_LEVEL.renderer = renderer;
+    window.GROUND_LEVEL.scene = scene;
+    window.GROUND_LEVEL.camera = camera;
+  }
+
   export default {
     name: 'GroundLevel',
     props: {
@@ -31,8 +46,31 @@
       window.GROUND_LEVEL = {};
 
       // Connect to the website.
-      const socket = io("https://cooperchickenbot.herokuapp.com/");
-      
+      const socket = io("https://cooperchickenbot.herokuapp.com/", {
+          // TODO: Add auth requirement later.
+          // auth: { token: "123" },
+          transports: ["websocket"]
+      });
+
+      // Debug socket opening/closing.
+      socket.on("connect", () => console.log('connect', socket.id));
+      socket.on("disconnect", () => console.log('disconnect', socket.id));
+      socket.on("data", data => console.log('data', data));
+
+      // Create basic scene.
+      generateGround();
+
+      // Random colour.
+
+      // Render a cube for the user.
+
+      // Render cubes for other people
+
+      // Add move (arrows) + WASD.
+
+
+      // Extra
+      // Load the profile picture for the user if they're logged in.
     }
   }
 </script>
