e2631b86c9b332e7305731fd1d33fe7173e10e74
LMF <LMFielding@liams-mbp.lan>
2021-07-07 16:07:29 +0100

    Add fight and a few placeholder images.

---
+++ pages/conquest/fight.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content-container">
+    <h1 class="title">Fight!</h1>
+    <h3>Thank you SleepySolaris for concept art!</h3>
+    <img 
+      class="fight-hero-image"
+      src="/conquest/fight.jpeg" alt="Conquest fight level thumbnail" />
+  </div>
+</template>
+
+<style scoped>
+  .fight-hero-image {
+    max-width: 100%;
+  }
+</style>
--- pages/conquest/index.vue
+++ pages/conquest/index.vue
@@ -2,24 +2,119 @@
   <div class="content-container">
     <h1 class="title">Conquest</h1>
 
-    <!-- <h2>Economy</h2> -->
-    <!-- <h2>Trades</h2> -->
-    <!-- <h2>Damage/attacks</h2> -->
+    <div class="conquest-menu">
+      <NuxtLink to="/conquest/world" class="conquest-menu-item">
+        <img 
+          class="conquest-menu-image"
+          src="/conquest/world.png" alt="Conquest world level thumbnail">
+        <span class="conquest-menu-item-link-label">
+          World Mode
+        </span>
+      </NuxtLink>
 
-    <p>
-      <NuxtLink to="/conquest/leaderboard">Leaderboard</NuxtLink>
-    </p>
+      <NuxtLink to="/conquest/ground" class="conquest-menu-item">
+        <img 
+          class="conquest-menu-image"
+          src="/conquest/ground.png" alt="Conquest ground level thumbnail">
+        <span class="conquest-menu-item-link-label">
+          Ground Mode
+        </span>
+      </NuxtLink>
 
-    <p>
-      <NuxtLink to="/conquest/world">World</NuxtLink>
-    </p>
+      <NuxtLink to="/conquest/fight" class="conquest-menu-item">
+        <img 
+          class="conquest-menu-image"
+          src="/conquest/fight.jpeg" alt="Conquest fight level thumbnail">
+        <span class="conquest-menu-item-link-label">
+          Fight
+        </span>
+      </NuxtLink>
 
-    <p>
-      <NuxtLink to="/conquest/ground">Ground</NuxtLink>
-    </p>
+      <NuxtLink 
+        to="/conquest/leaderboard"
+        class="conquest-menu-item">
+        <!-- <img 
+          srcset="elva-fairy-480w.jpg 480w"
+          sizes="(max-width: 600px) 480px,
+                  800px"
+          src="elva-fairy-800w.jpg"
+          alt="Elva dressed as a fairy"> -->
+        <span class="conquest-menu-item-link-label">
+          Leaderboard
+        </span>
+      </NuxtLink>
+
+      <div class="conquest-menu-item disabled">
+        <span 
+          class="conquest-menu-item-link-label"
+          to="/conquest/leaderboard">Info: Economy</span>
+      </div>
+
+      <div class="conquest-menu-item disabled">
+        <span 
+          class="conquest-menu-item-link-label"
+          to="/conquest/leaderboard">Trade</span>
+      </div>
+
+      <div class="conquest-menu-item disabled">
+        <span 
+          class="conquest-menu-item-link-label"
+          to="/conquest/leaderboard">Info: Conflict</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {}
 </script>
+
+<style scoped>
+  .conquest-menu {
+    display: flex;
+    flex-wrap: wrap;
+    
+    margin-top: 2.5rem;
+  }
+
+  .conquest-menu-item {
+    height: 11em;
+    flex: 0 0 50%;
+
+    font-size: 1.35em;
+    
+    position: relative;
+  }
+
+  .conquest-menu-image {
+    position: absolute;
+    left: 0;
+    top: 0;
+
+    max-width: 100%;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .conquest-menu-item-link-label {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    color: #ff6565;
+  }
+  .conquest-menu-item:hover .conquest-menu-item-link-label, .conquest-menu-item:focus .conquest-menu-item-link-label {
+    color: #111111;
+  }
+  .conquest-menu-item:hover .conquest-menu-image, .conquest-menu-item:focus .conquest-menu-image {
+    opacity: .25;
+  }
+  .conquest-menu-item:visited .conquest-menu-item-link-label {
+    text-decoration: none;
+  }
+  .conquest-menu-item.disabled {
+    cursor: not-allowed;
+  }
+</style>
