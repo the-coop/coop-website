e9ddb03a7419eb55db7bc68f18e716062e028929
LMF <contact@lmfielding.com>
2021-08-06 22:30:26 +0100

    Fight mode!

---
+++ components/fight/FightGame.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="fightgame-wrapper">
+    <div class="fightgame" />
+    <div class="ui"></div>
+  </div>
+</template>
+
+<style>
+  .fightgame {
+    width: 100vw;
+    height: 100vh;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'Fightgame',
+    props: {
+      tile: {
+        type: String,
+        default: null
+      }
+    },
+    methods: {},
+    async mounted() {
+      alert('Fight game should start.')
+    }
+  }
+</script> 
