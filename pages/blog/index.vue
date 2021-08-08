508a0d410f1c4040e2a1b3a8aaa01755b52b723f
LMF <contact@lmfielding.com>
2021-08-08 01:32:15 +0100

    Add blog parsing and markdown ability.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,5 +4,20 @@
       <h1 class="title">Our blog will be launching here!</h1>
       <p>Stay tuned! Join our newsletter here... (when link is added ;))</p>
     </div>
+
+    <!-- <nuxt-content :document="page" /> -->
   </div>
 </template>
+
+<script>
+  import blogParser from '~/lib/blog/blogParser';
+
+  export default {
+    // async asyncData({ $axios }) {
+    //   const resp = await $axios.get('/path/to/example.md');
+    //   const page = await blogParser(resp.data);
+
+    //   return { page };
+    // }
+  }
+</script>
