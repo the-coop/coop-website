a4813b6ba28eb504a75e723d6d06ae04cf1af67f
L M Fielding <contact@lmfielding.com>
2022-03-08 23:47:18 +0000

    Add more error highlighting

--- pages/conquest/economy/trade/add.vue
+++ pages/conquest/economy/trade/add.vue
@@ -1,21 +1,31 @@
 <template>
+  <!-- TODO: -->
+  <!-- Add form validation -->
+  <!-- Make form item code fields upper case only -->
+  <!-- Clear/empty values for retry -->
+
   <div class="content-container">
-    <h1 v-if="!processing" class="title">Create a trade</h1>
+    <h1 v-if="!processing && !success" class="title">Create a trade</h1>
     <h1 v-if="processing" class="title">Submitting your trade</h1>
+    <h1 v-if="success" class="title">Trade submitted</h1>
  
     <div v-if="$auth.$state.loggedIn">
-      <form :class="`form ${processing ? 'disabled' : ''}`">
+      <form v-if="!success" :class="`form ${processing ? 'disabled' : ''}`">
         <div class="fieldset">
           <span class="fieldset-title">Offering</span>
 
           <div class="input">
             <span class="input-label">Offering Item</span>
-            <input v-model="offer_item" :disabled="processing" class="input-target" type="text" name="offer_item" placeholder="[ITEM_CODE]" />
+            <input required v-model="offer_item" :disabled="processing" 
+              :class="`input-target ${errors.invalid_offer_item ? 'errored' : ''}`"
+              type="text" name="offer_item" placeholder="[ITEM_CODE]" />
           </div>
 
           <div class="input">
             <span class="input-label">Offering Amount</span>
-            <input v-model="offer_qty" :disabled="processing" class="input-target" placeholder="1" type="number" name="offer_qty" />
+            <input v-model="offer_qty" :disabled="processing" 
+            :class="`input-target ${errors.invalid_offer_qty ? 'errored' : ''}`" 
+            placeholder="1" type="number" name="offer_qty" />
           </div>
         </div>
 
@@ -24,21 +34,29 @@
 
           <div class="input">
             <span class="input-label">Attaining Item</span>
-            <input v-model="receive_item" :disabled="processing" class="input-target" type="text" name="receive_item" placeholder="[ITEM_CODE]" />
+            <input required v-model="receive_item" :disabled="processing" 
+              :class="`input-target ${errors.invalid_receive_item ? 'errored' : ''}`" 
+              type="text" name="receive_item" placeholder="[ITEM_CODE]" />
           </div>
 
           <div class="input">
             <span class="input-label">Attaining Amount</span>
-            <input v-model="receive_qty" :disabled="processing" class="input-target"  placeholder="1" type="number" name="receive_qty" />
+            <input v-model="receive_qty" :disabled="processing" 
+              :class="`input-target ${errors.invalid_receive_qty ? 'errored' : ''}`"
+              placeholder="1" type="number" name="receive_qty" />
           </div>
         </div>
       </form>
 
+      <h2 class="subtitle" v-if="success">
+        Your trade was successfully submit, use the buttons below to return to listings or place another trade.
+      </h2>
+
       <NuxtLink to="/conquest/economy/trade">
         <button class="button secondary">Listings</button>
       </NuxtLink>
-      <button class="button" v-on:click="add">Confirm</button>
-      <!-- <button class="button" v-on:click="add">Confirm</button> -->
+      <button v-if="!success" class="button" v-on:click="add">Confirm</button>
+      <button v-if="success" class="button" v-on:click="reset">Reset</button>
     </div>
 
     <div v-if="!$auth.$state.loggedIn">
@@ -55,6 +73,10 @@
   .form {
     margin-top: 2em;
   }
+
+  .errored {
+    background-color: #5b0000;
+  }
 </style>
 
 <script>
@@ -64,22 +86,35 @@
   export default {
     data() {
       return { 
-        trade: null,
         processing: false,
 
         offer_item: null,
         offer_qty: 1,
         receive_item: null,
-        receive_qty: 1
+        receive_qty: 1,
+
+        success: false,
+        errors: {}
       };
     },
     components: { PopupWrapper },
     methods: {
+      reset() {
+        // processing: false,
+
+        this.offer_item = null;
+        this.offer_qty = 1;
+        this.receive_item = null;
+        this.receive_qty = 1;
+
+        this.success = false;
+        this.errors = {};
+      },
       async add() {
+        // Disable re-submission whilst processing.
         this.processing = true;
 
-        // console.log(this.offer_item, this.offer_qty, this.receive_item, this.receive_qty);
-
+        // Attempt trade order.
         const response = await fetch(API.BASE_URL + 'trades/create', {
           method: 'POST',
           body: JSON.stringify({
@@ -93,15 +128,20 @@
             "Authorization": this.$auth.strategy.token.get()
           }
         });
-        
-        console.log(response);
+        const data = await response.json();
+        console.log(data);
+
+        // Handle validation.
+        if (data.success)
+          this.success = true;
+        else
+          this.errors = data.errors;
+
+        // console.log(this.errors);
+
+        // Reallow form interaction.
+        this.processing = false;
       }
-    },
-    async mounted() {
-      // const tradesResp = await API.get('economy/trades');
-      // const trades = tradesResp.data;
-      // this.trades = trades;
-      // console.log(trades)
     }
   }
 </script>
