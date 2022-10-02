294f46610c782a1f51d511752eaf20d565c0a997
L M Fielding <contact@lmfielding.com>
2022-10-02 04:53:29 +0100

    Add roles page

---
+++ pages/members/roles.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="roles">
+    Roles
+
+    <h2>You will soon be able to set Discord roles here.</h2>
+    <p>This will save us a lot of space on the server and be much more clear/intuitive.</p>
+  </div>
+</template>
+
+
+<script>
+</script>
+
+
+
+<style scoped>
+  .roles {
+
+  }
+
+  @media (min-width: 800px) {
+
+  }
+</style>
