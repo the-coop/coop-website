4e489fda1f3661d7327ad7c8807b7201e9e45f41
onotaizee@gmail.com <onotaizee@gmail.com>
2023-02-20 00:04:50 +0100

    +added: twemoji svgs +added: named svgs based on discord name

--- pages/guide/sacrifice/index.vue
+++ pages/guide/sacrifice/index.vue
@@ -7,14 +7,14 @@
       <p class="text">
         TheCoop sacrifice ritual is an event that occurs when a condition is fulfilled. The condition
         being that at least <span class="var">5%</span> of the entire server reacts to a user's message with the
-        dagger emoji(🗡)
+        dagger emoji <img :src="twemote('dagger')" alt="dagger" class="emoji">
       </p>
       <p class="text">
         When this happens, cooper sends a message in chat which begins the sacrifice of the player
       </p>
       <p class="quote-text">Cooper message</p>
       <p class="text">
-        Cooper then reacts to this message with a shield(🛡) and a dagger(🗡). This acts as a voting poll
+        Cooper then reacts to this message with a shield(<img :src="twemote('shield')" alt="shield" class="emoji">) and a dagger(<img :src="twemote('dagger')" alt="dagger" class="emoji">). This acts as a voting poll
         on wether to sacrifice the member or not. Clicking on the dagger is a vote to sacrifice, whereas clicking on the
         shield is a vote to spare the member
       </p>
@@ -26,7 +26,7 @@
             <span class="quote-text">Cooper</span> will send the following message in chat:
 
             <p class="quote-text">
-              <span class="var">User</span> you cannot vote for/against yourself. 🗡
+              <span class="var">User</span> you cannot vote for/against yourself. <img :src="twemote('dagger')" alt="dagger" class="emoji">
             </p>
           </li>
           <li class="unordered-list">
@@ -34,7 +34,7 @@
             <span class="quote-text">Cooper</span> will send the following message in chat:
 
             <p class="quote-text">
-              <span class="var">User</span> you cannot vote as a PROSPECT. 🗡
+              <span class="var">User</span> you cannot vote as a PROSPECT. <img :src="twemote('dagger')" alt="dagger" class="emoji">
             </p>
           </li>
         </ul>
@@ -42,7 +42,10 @@
       <p class="text">
         When a member is sacrificed, the person is banned from <span class="var">THECOOP™</span>, this is especially
         useful because spammers can be banned without admin intervention and instead
-        via sweet, sweet democracy ... ☕🧐
+        via sweet, sweet democracy ... <img :src="twemote('tea')" alt="tea" class="emoji"><img :src="twemote('spockle')" alt="spockle" class="emoji">
+      </p>
+      <p id="emojis">
+        🥚🧺🐰
       </p>
     </div>
   </div>
@@ -57,9 +60,16 @@
     },
 
     async mounted(){
-      const paragraphs = document.querySelectorAll('p')
-      for(let paragraph of paragraphs){
-        twemoji.parse(paragraph)
+      const span = document.getElementById('emojis')
+      twemoji.parse(span, {
+        callback: (icon) => {
+          console.log(icon)
+        }
+      })
+    },
+    methods: {
+      twemote: (emoji) => {
+        return require(`../../../assets/twemojis/${emoji}.svg`)
       }
     }
   }
