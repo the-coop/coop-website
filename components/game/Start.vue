c4d6cc1195048a97de7f6bbba8609db0f0bfdf8c
L M Fielding <contact@dreamsbyte.com>
2024-12-03 23:15:15 -0300

    pure

--- components/game/Start.vue
+++ components/game/Start.vue
@@ -1,12 +1,54 @@
 <template>
   <div class="start">
-    <span @click="$emit('start')">Click to Play</span>
+    <span @click="start">Click to Play</span>
+
+    <!-- Display connected gamepads -->
+    <div v-if="gamepads.length">
+      <h3>Connected Gamepads:</h3>
+      <div v-for="(gp, index) in gamepads" :key="index">
+        {{ gp.id }} - {{ gp.buttons.filter(btn => btn.pressed).length }} buttons pressed
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'Start',
+    props: {
+      gamepads: { type: Array, default: () => [] },
+      start: { type: Function, required: true }
+    },
+    data() {
+      return {
+        listening: false
+      };
+    },
+    mounted() {
+      this.listening = true;
+      this.listen();
+    },
+    methods: {
+      listen() {
+        // Flag for stopping/cleaning up.
+        if (!this.listening) return;
+
+        // Check gamepads for button presses.
+        navigator.getGamepads().map(gp => {
+          if (gp && gp.buttons.some(button => button.pressed)) {
+            console.log(button, button.pressed);
+
+            this.start();
+            this.listening = false;
+          }
+        });
+
+        requestAnimationFrame(this.listen);
+      }
+    },
+    unmounted() {
+      this.listening = false;
+    },
   };
 </script>
 
--- pages/game.vue
+++ pages/game.vue
@@ -1,74 +1,75 @@
 <template>
   <div class="game">
-    <Start v-if="!started" @start="startGame" />
+    <Start v-if="!started" :gamepads="gamepads" :start="start" />
     <canvas class="viewer" ref="canvas"></canvas>
   </div>
 </template>
 
 <script>
-import Engine from '../lib/game/engine.mjs';
-import Start from '../components/game/Start.vue';
+  import Engine from '../lib/game/engine.mjs';
+  import Start from '../components/game/Start.vue';
+  import Gamepad from '../lib/game/inputs/gamepad.mjs';
 
-export default {
-  name: 'Game',
-  components: {
-    Start,
-  },
-  data() {
-    return {
-      started: false
-    };
-  },
-  methods: {
-    startGame() {
-      this.started = true;
+  export default {
+    name: 'Game',
+    components: {
+      Start
+    },
+    data() {
+      return {
+        started: false,
+        gamepads: []
+      };
+    },
+    methods: {
+      start() {
+        this.started = true;
+      }
+    },
+    mounted() {
       // Setup renderer, world, camera, basics.
-      Engine.setup(this.$refs.canvas);
+      Engine.setup(this);
 
       // Temp testing.
       Engine.cube = Engine.createCube();
       Engine.scene.add(Engine.cube);
 
       // Gamepad input needs to be ready for start screen.
+      Gamepad.setup();
 
       // Start game loop.
       Engine.loop();
+    },
+    unmounted() {
+      Engine.cleanup();
+      Gamepad.cleanup();
     }
-  },
-  mounted() {
-    if (this.started) {
-      this.startGame();
-    }
-  },
-  unmounted() {
-    Engine.cleanup();
-  },
-};
+  };
 </script>
 
 <style scoped>
-.game {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  .game {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  height: 100dvh;
-  width: 100vw;
+    height: 100dvh;
+    width: 100vw;
 
-  position: absolute;
-  top: 0;
-  left: 0;
-  background: #000;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: #000;
 
-  overflow: hidden;
-}
+    overflow: hidden;
+  }
 
-.viewer {
-  width: 100%;
-  height: 100%;
-}
+  .viewer {
+    width: 100%;
+    height: 100%;
+  }
 
-.start {
-  /* ...existing styles... */
-}
+  .start {
+    /* ...existing styles... */
+  }
 </style>
