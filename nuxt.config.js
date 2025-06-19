887cfdf65c128a4422de177164d1c76aa81bf4e0
lmf-git <contact@dreamsbyte.com>
2025-06-19 01:11:33 -0300

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,14 @@
 export default defineNuxtConfig({
-  ssr: true,
   nitro: {
-    preset: 'static' // Use static preset for Netlify
+    preset: 'static',
+    output: {
+      publicDir: 'dist'
+    }
   },
   vite: {
     server: {
       fs: {
-        // Suppress development shared repo errors because it doesn't work with yarn link
-        strict: false 
+        strict: false // Disable strict file serving restrictions
       }
     }
   }
